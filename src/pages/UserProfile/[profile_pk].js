661a245c5f85d516fc68a6e756ff6ab374142249
Matthew Wrobel <mwrobel2002@yahoo.com>
2023-11-30 00:04:24 -0500

    user_prof_branch_2: added a few notes for remaining dev work

--- src/pages/UserProfile/[profile_pk].js
+++ src/pages/UserProfile/[profile_pk].js
@@ -11,6 +11,8 @@ const[user,loading,error] = useAuthState(auth);
 const [curName, setCurName] = useState("");
 const [curEmail, setCurEmail] = useState("");
 
+// TODO: SET THE CURRENT NAME, EMAIL, PROFILE PICTURE ON PAGE LOAD to initial values based on the user? Maybe need to do this via useEffect(user) to call a function when user is obtained, dunno tbh been a while for react syntax
+
 const [editing, setEditing] = useState(false);
 
 const setEditingHandler = () => {
@@ -26,6 +28,7 @@ const setEditingHandler = () => {
       setEditing(false)
       // ...
     }).catch((error) => {
+      // TODO: display error to user, dont need anything super fancy here, can just set a variable const [formHasError, setFormHasError] = useState(false), and do if (formHasError) down in edit form section like how we do if(editing).
       // An error occurred
       console.log("ERROR OCCURED !", error)
       // ...
@@ -47,8 +50,11 @@ console.log("USER", user)
 
 if (user) {
   if (editing){
+    // Edit form section
     return (
       <div>
+        {/* USE SIMILAR FRAMEWORK FOR NAME, EMAIL, PROFILE PICTURE. profile picture can be text input or maybe file input, unsure how that works. text input probably easier since we arent 
+        storing actual images, just the urls. also simplifies things. then frontend for it in  */}
           <h1>User Profile</h1>
           <h2>Name: </h2><input id="edit_name" type="text" onChange={setNameHandler} value={curName}></input>
           <h2>Email: </h2><input id="edit_email" type="text" onChange={setEmailHandler} value={curEmail}></input>
@@ -58,6 +64,8 @@ if (user) {
       </div>
     );
   } else {
+    // Displaying details, not editing section.
+    // Image frontend here can use the curUrl as value, make sure to limit the size of the image so huge images don't murder the aesthetic
     return (
       <div>
           <h1>User Profile</h1>
